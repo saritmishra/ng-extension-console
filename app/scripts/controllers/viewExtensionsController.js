fe9c48407a0ff43b0b0e23f118b86869cf711093
Sarit Mishra <sarit.mishra@gmail.com>
2014-09-15 22:18:15 -0700

    adding functionality to "create" and hooking it with "view"

--- app/scripts/controllers/viewExtensionsController.js
+++ app/scripts/controllers/viewExtensionsController.js
@@ -4,7 +4,7 @@
 
     var viewExtensionsController = function ($scope, extensionConsoleFactory) {
 
-        var model = $scope.model = {"extensionList" : [{ "name" : "app-name", "id" : "ID001" }, { "name" : "app-name2", "id" : "ID002" }]};
+        var model = $scope.model = {};
 
             // model.bestSellers = [];
             // model.personalRatings = []; //Not very useful - Not linked to individual books and lost after page-refresh
@@ -14,44 +14,44 @@
             // model.rating ="2";
 
         model.getExtensionDetails = function(extension) {
-            // return
+            return extensionConsoleFactory.extensionList;
         };
 
-        model.getCategories = function() {
-            return bestSellerFactory.getCategories()
-                        .get(function(results) {
-                            model.categories = results.results;
-                        });
-        };
+        // model.getCategories = function() {
+        //     return bestSellerFactory.getCategories()
+        //                 .get(function(results) {
+        //                     model.categories = results.results;
+        //                 });
+        // };
 
-        model.getDetail = function (book, detail) {
-            return book.book_details[0][detail];
-        };
+        // model.getDetail = function (book, detail) {
+        //     return book.book_details[0][detail];
+        // };
 
-        model.fetchBestSellers = function() {
-            model.currentPage = 1;
-            model.getDataFor(model.selectedCategory.list_name, model.currentPage);
-        };
+        // model.fetchBestSellers = function() {
+        //     model.currentPage = 1;
+        //     model.getDataFor(model.selectedCategory.list_name, model.currentPage);
+        // };
 
-        model.fetchBestSellersNextPage = function() {
-            model.getDataFor(model.selectedCategory.list_name, model.currentPage);
-        };
+        // model.fetchBestSellersNextPage = function() {
+        //     model.getDataFor(model.selectedCategory.list_name, model.currentPage);
+        // };
 
-        model.getDataFor = function (category, offset) {
-            if (!category)
-                return;
-            model.personalRatings = [];
-            category = category.split(" ").join("-");
+        // model.getDataFor = function (category, offset) {
+        //     if (!category)
+        //         return;
+        //     model.personalRatings = [];
+        //     category = category.split(" ").join("-");
 
-            offset = (offset) ? (offset - 1) * 20 : 1;
+        //     offset = (offset) ? (offset - 1) * 20 : 1;
 
-            bestSellerFactory.getBestSellers(category, offset)
-               .get(function (results) {
-                   model.bestSellers = results.results;
-                   model.copyright = results.copyright;
-                   model.totalItems = results.num_results;
-               });
-        };
+        //     bestSellerFactory.getBestSellers(category, offset)
+        //        .get(function (results) {
+        //            model.bestSellers = results.results;
+        //            model.copyright = results.copyright;
+        //            model.totalItems = results.num_results;
+        //        });
+        // };
 
         // var init = function() {
         //     model.getCategories().$promise //this is calling model.getCategories() first, before handing over the $promise
--- app/scripts/services/extensionConsoleFactory.js
+++ app/scripts/services/extensionConsoleFactory.js
@@ -6,13 +6,20 @@
     var extensionConsoleFactory = function($resource) {
         var factory = {};
 
-        factory.getBestSellers = function(category, offset){
-            return $resource("/getBestSellers/" + category + "/" + offset);
+        factory.extensionList =  [{ "name" : "app-001", "groupID" : "G001",  "artifactID" : "A001",  "releaseID" : "R001" },
+                                              { "name" : "app-002", "groupID" : "G002",  "artifactID" : "A002",  "releaseID" : "R002" }];
+
+        factory.addExtension = function(extension){
+            factory.extensionList.push(extension);
         };
 
-        factory.getCategories = function(){
-            return $resource("/getCategories");
-        };
+        // factory.getBestSellers = function(category, offset){
+        //     return $resource("/getBestSellers/" + category + "/" + offset);
+        // };
+
+        // factory.getCategories = function(){
+        //     return $resource("/getCategories");
+        // };
 
         return factory;
     };
